f1913852fc3439a5cb6ee8f43766ba88e4feae79
Mickaël Andrieu <andrieu.travail@gmail.com>
2017-10-03 14:52:41 +0200

    BO: fixed Codacy warning

--- admin-dev/themes/default/js/bundle/admin_parameters/performancePageUI.js
+++ admin-dev/themes/default/js/bundle/admin_parameters/performancePageUI.js
@@ -24,10 +24,10 @@
  */
 var PerformancePageUI = {
     displaySmartyCache: function() {
-        var CACHE_ENABLED = 1;
+        var CACHE_ENABLED = '1';
         var smartyCacheSelected = document.getElementById('form_smarty_cache').value;
         var smartyCacheOptions = document.querySelectorAll('.smarty-cache-option');
-        if (smartyCacheSelected == CACHE_ENABLED) {
+        if (smartyCacheSelected === CACHE_ENABLED) {
           for(var i = 0; i < smartyCacheOptions.length; i++) {
             smartyCacheOptions[i].style.display = 'block';
           }
@@ -40,11 +40,11 @@ var PerformancePageUI = {
       }
     },
     displayCacheSystems: function() {
-        var CACHE_ENABLED = 1;
+        var CACHE_ENABLED = '1';
         var cacheEnabledInput = document.getElementById('form_caching_use_cache');
         var cachingElements = document.getElementsByClassName('memcache');
 
-        if(cacheEnabledInput.value == CACHE_ENABLED) {
+        if(cacheEnabledInput.value === CACHE_ENABLED) {
             for (var i = 0; i < cachingElements.length; i++) {
                 cachingElements[i].style.display = "block";
             }
@@ -58,14 +58,14 @@ var PerformancePageUI = {
         }
     },
     displayMemcacheServers: function() {
-        var CACHE_ENABLED = 1;
+        var CACHE_ENABLED = '1';
         var cacheEnabledInput = document.getElementById('form_caching_use_cache');
         var cacheSelected = document.querySelector('input[name="form[caching][caching_system]"]:checked').value;
         var memcacheServersListBlock = document.getElementById('servers-list');
 
         if (
-            (cacheSelected == "CacheMemcache" || cacheSelected == "CacheMemcached")
-            && cacheEnabledInput.value == CACHE_ENABLED
+            (cacheSelected === "CacheMemcache" || cacheSelected === "CacheMemcached")
+            && cacheEnabledInput.value === CACHE_ENABLED
         ) {
             memcacheServersListBlock.style.display = "block";
 
